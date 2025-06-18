225dbac781fdfd24f4b41c8513ee26eaa146154b
zik30 <ainazik.ishenbek@gmail.com>
2025-06-18 16:45:21 +0600

    initial

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,14 +1,18 @@
 import { type FC, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Footer } from 'widgets/footer';
+import { Header } from 'widgets/header';
 
 export const Layout: FC = () => {
     return (
         <>
+            <Header />
             <Suspense fallback={<div> Loading....</div>}>
                 <main>
                     <Outlet />
                 </main>
             </Suspense>
+            <Footer />
         </>
     );
 };
